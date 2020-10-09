b59470df3f8156a5b36b5b8778ff6fdb0cd52ff2
nayimovag <72025248+nayimovag@users.noreply.github.com>
2020-10-09 21:47:57 +0500

    Add files via upload

--- myFile.py
+++ myFile.py
@@ -1,1 +1,10 @@
-print("hello worlld")
+list = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list[2][2].append(7000)
+print(list)
+
+list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
+#["h", "i", "j"]
+#list1[2][1][2].append("h", "i", "j")
+list2=['h', 'i', 'j']
+list1[2][1][2].extend(list2)
+print(list1)
